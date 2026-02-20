649d564f83e1e978dec2562569df7b0df56c9b75
松野佑哉 <yuya@domo.mp>
2026-02-20 17:48:54 +0900

    デプロイテスト

--- src/app/page.js
+++ src/app/page.js
@@ -22,7 +22,7 @@ export default async function Home() {
           <h1 className="text-3xl font-bold text-red-500">{siteName}</h1>
           <p>{data.profile?.COMPANY}</p>
           <p>
-            Looking for a starting point or more instructions? Head over to{" "}
+            22222Looking for a starting point or more instructions? Head over to{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               target="_blank"
